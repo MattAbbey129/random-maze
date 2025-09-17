09029febe181d91fa47e688984c092e22b573f99
MattAbbey129 <Borrowing_Lushness974@SimpleLogin.com>
2025-09-16 20:26:16 -0400

    build: add Nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: MIT
+# Copyright © 2025 Matt Abbey
+
+{
+
+  description = "Generates a random maze using Bash.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        my-name = "random-maze";
+        my-buildInputs = with pkgs; [ bash coreutils-full ];
+        my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./script.bash)).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+      in rec {
+        defaultPackage = packages.my-script;
+        packages.my-script = pkgs.symlinkJoin {
+          name = my-name;
+          paths = [ my-script ] ++ my-buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+        };
+      }
+    );
+
+}
